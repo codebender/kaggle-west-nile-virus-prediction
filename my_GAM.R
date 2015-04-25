38198cddbac8d058a7a36e325383dd1896da0e86
Matthew Bender <benderm@gmail.com>
2015-04-25 13:24:02 -0600

    add week, not for smoothing
    score: 0.74826
    
    * rename x to train

--- my_GAM.R
+++ my_GAM.R
@@ -1,12 +1,12 @@
 library(Metrics)
 library(gam)
 library(data.table)   ## load data in quickly with fread
-x <- fread("Data/train.csv")
+train <- fread("Data/train.csv")
 test <- fread("Data/test.csv")
 
 ## prep the species column by moving the test-only UNSPECIFIED CULEX to CULEX ERRATICUS, and re-doing the levels
 ## logistic regression will complain otherwise
-vSpecies<-c(as.character(x$Species),as.character(test$Species))
+vSpecies<-c(as.character(train$Species),as.character(test$Species))
 vSpecies[vSpecies=="UNSPECIFIED CULEX"]<-"CULEX ERRATICUS"
 vSpecies[-which(vSpecies == "CULEX PIPIENS" |
                   vSpecies == "CULEX PIPIENS/RESTUANS" |
@@ -14,42 +14,36 @@ vSpecies[-which(vSpecies == "CULEX PIPIENS" |
 vSpecies<-factor(vSpecies,levels=unique(vSpecies))
 
 ## data.table syntax for adding a column; could overwrite the existing column as well
-x[,Species2:=factor(vSpecies[1:nrow(x)],levels=unique(vSpecies))]
-test[,Species2:=factor(vSpecies[(nrow(x)+1):length(vSpecies)],levels=unique(vSpecies))]
+train[,Species2:=factor(vSpecies[1:nrow(train)],levels=unique(vSpecies))]
+test[,Species2:=factor(vSpecies[(nrow(train)+1):length(vSpecies)],levels=unique(vSpecies))]
 
-## also add some fields for components of the date using simple substrings
-x[,dMonth:=as.factor(paste(substr(x$Date,6,7)))]
-x[,dYear:=as.factor(paste(substr(x$Date,1,4)))]
-x$Date = as.Date(x$Date, format="%Y-%m-%d")
-xsDate = as.Date(paste0(x$dYear, "0101"), format="%Y%m%d")
-x$dWeek = as.numeric(paste(floor((x$Date - xsDate + 1)/7)))
-x$Block <- as.factor(x$Block)
+## date fields
+train$week <- factor(format(as.POSIXct(train$Date, format="%Y-%m-%d"), format="%U"))
+test$week <- factor(format(as.POSIXct(test$Date, format="%Y-%m-%d"), format="%U"))
+train$year <- format(as.POSIXct(train$Date, format="%Y-%m-%d"), format="%Y")
+test$year <- format(as.POSIXct(test$Date, format="%Y-%m-%d"), format="%Y")
 
-test[,dMonth:=as.factor(paste(substr(test$Date,6,7)))]
-test[,dYear:=as.factor(paste(substr(test$Date,1,4)))]
-test$Date = as.Date(test$Date, format="%Y-%m-%d")
-tsDate = as.Date(paste0(test$dYear, "0101"), format="%Y%m%d")
-test$dWeek = as.numeric(paste(floor((test$Date - tsDate + 1)/7)))
+## fix Blocks data
+train$Block <- as.factor(train$Block)
 test$Block <- as.factor(test$Block)
 test$Block[test$Block == 26] <- 25
 
 # we'll set aside 2011 data as test, and train on the remaining
-my.x = data.frame(x[,list(WnvPresent, dWeek, Block, Species2, Latitude, Longitude)])
-x1<-my.x[x$dYear!=2011,]
-x2<-my.x[x$dYear==2011,]
-x2$Block[x2$Block==29] <- 28
-x2$Block[x2$Block==34] <- 33
+my.train = data.frame(train[,list(WnvPresent, week, Block, Species2, Latitude, Longitude)])
+train1<-my.train[train$year != 2011,]
+train2<-my.train[train$year == 2011,]
+train2$Block[train2$Block==29] <- 28
+train2$Block[train2$Block==34] <- 33
 
 ## GAM modelling
-require(gam)
-fitCv = gam(WnvPresent ~ s(dWeek) + Block + Species2 + lo(Latitude, Longitude),
-           data = x1, family="binomial")
-p2<-predict(fitCv, newdata = x2, type = "response")
+fitCv = gam(WnvPresent ~ week + Block + Species2 + lo(Latitude, Longitude),
+           data = train1, family="binomial")
+p2<-predict(fitCv, newdata = train2, type = "response")
 ## check for a reasonable AUC of the model against unseen data (2011)
-auc(x2$WnvPresent,p2)
+auc(train2$WnvPresent,p2)
 
 ## now fit a new model to all the data, so that our final submission includes information learned from 2011 as well
-fitSubmit <- update(fitCv, data=my.x)
+fitSubmit <- update(fitCv, data=my.train)
 pSubmit<-predict(fitSubmit, newdata = test, type = "response")
 ## look at the predicted distribution (AUC doesn't care about probabilities; just ordering. It's still a good diagnostic)
 summary(pSubmit)
@@ -57,4 +51,4 @@ summary(pSubmit)
 submissionFile<-cbind(test$Id,pSubmit)
 colnames(submissionFile)<-c("Id","WnvPresent")
 options("scipen"=100, "digits"=8)
-write.csv(submissionFile,"Output/GAM_with_block.csv",row.names=FALSE,quote=FALSE)
+write.csv(submissionFile,"Output/GAM_with_block_2.csv",row.names=FALSE,quote=FALSE)
